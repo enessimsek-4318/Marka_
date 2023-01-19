6f7456e508ccb85718331760acf721a6118ba94b
enessimsek-4318 <101976848+enessimsek-4318@users.noreply.github.com>
2023-01-19 15:08:11 +0300

    _Navbar and _Layout view pages have been added.

---
+++ Marka_WebUI/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Marka_WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Marka_WebUI/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Marka_WebUI.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
