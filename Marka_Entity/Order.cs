134c5f83342f6c4e4a780050d0ffa5aed021e4dc
enessimsek-4318 <101976848+enessimsek-4318@users.noreply.github.com>
2023-02-20 11:04:22 +0300

    Order and OrderItem Entity have been created.

--- Marka_Entity/Order.cs
+++ Marka_Entity/Order.cs
@@ -8,11 +8,38 @@ namespace Marka_Entity
 {
     public class Order
     {
-        public int MyProperty { get; set; }
+        public int Id { get; set; }
+        public string OrderNumber { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string UserId { get; set; }
+        public EnumOrderState OrderState { get; set; }
+        public EnumPaymentTypes PaymentTypes { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string OrderNote { get; set; }
+        public string PaymentId { get; set; }
+        public string ConversationId { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; }
+
+        public Order()
+        {
+            OrderItems = new List<OrderItem>();
+        }
     }
-
-    public class OrderLine
+    public enum EnumOrderState
     {
-
+        waiting = 0,
+        Unpaid = 1,
+        Completed = 2
+    }
+    public enum EnumPaymentTypes
+    {
+        CreditCart = 0,
+        Eft = 1
     }
 }
