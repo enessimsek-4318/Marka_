d617f8882445b6d039b8bb7336716949c0c8247e
enessimsek-4318 <101976848+enessimsek-4318@users.noreply.github.com>
2023-02-22 15:30:31 +0300

    WebAPI has been included in the project.

---
+++ Marka_WebAPI/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Marka_WebAPI.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Marka_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ApplicationUser>>> GetUsers()
+        {
+            if (_userManager.Users!=null)
+            {
+                return await _userManager.Users.ToListAsync();
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ Marka_WebAPI/Identity/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Marka_WebAPI.Identity
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string FullName { get; set; }
+    }
+}
